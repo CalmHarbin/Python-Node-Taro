726e91e5201c92ed31b0e992371c48a7e32d83ce
CalmHarbin <363639121@qq.com>
2019-08-11 14:09:31 +0800

    爬虫新增代理池

--- ip/index.py
+++ ip/index.py
@@ -1,60 +1,79 @@
 import requests
+import random
 from pymongo import MongoClient
 from lxml import etree  # xpath解析模块
-import threading
-import time
 from concurrent.futures import ThreadPoolExecutor  # 线程池
+
 
 # 数据库连接
 client = MongoClient('127.0.0.1', 27017)
 db = client.ip  # 连接mydb数据库，没有则自动创建
-table = db.table  # 使用test_set集合，没有则自动创建
+table = db.table  # 将抓取的ip全部存到table表中
+ip_table = db.ip  # 有效ip表
 
+# 线程池
 spider_poll_max = 5  # 爬虫线程池最大数量
-spider_poll = ThreadPoolExecutor(
-    max_workers=spider_poll_max)  # 爬虫线程池 max_workers最大数量
-
-
-# 获取id
-
-
-def getIp(page):
-    print(page)
-    # ['112.85.151.3:9999', '103.114.10.246:8080', '36.89.192.115:60297', '14.207.31.178:8080', '202.61.49.52:48713', '119.82.253.210:58069', '47.106.59.75:3128', '189.26.121.206:3128']
-    proxies = {
-        # 'http': '112.85.151.3:9999',
-        # 'https': '112.85.151.3:9999'
-    }
-    # 请求数据
-    response = requests.get(
-        'http://www.xiladaili.com/gaoni/' + str(page + 1)+'/', proxies=proxies)
-    # 设置编码
-    response.encoding = 'utf-8'
-    # 解析
-    s = etree.HTML(response.text)
-    # 获取ip和请求类型
-    ips = s.xpath(
-        '/html/body/div[1]/div[3]/div[2]/table/tbody/tr/td[1]/text()')
-    types = s.xpath(
-        '/html/body/div[1]/div[3]/div[2]/table/tbody/tr/td[2]/text()')
-    print(ips)
-    print(types)
-    # 验证ip的有效性
-    for index, ip in enumerate(ips):
-        host = ip.split(':')[0]
-        port = ip.split(':')[1]
-        table.insert_one({"ip": host, "port": port, "type": types[index]})
+# 爬虫线程池 max_workers最大数量
+spider_poll = ThreadPoolExecutor(max_workers=spider_poll_max)
 
 
 def getRandomIp():
-    global table
+    '''
+        @method 有效ip表中随机取一个ip
+    '''
+    global ip_table
     # 获取总数量
-    count = table.find({}, {'ip': 1}).count()  # 查询name为lisi的数据的数量
+    count = ip_table.count_documents({})  # 查询一共有多少数据
     # 随机取一个
+    index = random.randint(0, count)  # 获取0到count的随机整数
+    data = ip_table.find().skip(index).limit(1)
 
-    # 爬取1000页
+    for item in data:
+        return {'ip': item['ip'], 'port': item['port']}
 
 
-for page in range(0, 5):
+def getIp(page):
+    '''
+        @method 爬取数据
+    '''
+    # 随机获取一个代理,防止被封ip
+    row = getRandomIp()
+    proxies = {
+        'http': row['ip'] + ':' + row['port'],
+        'https': row['ip'] + ':' + row['port']
+    }
+    try:
+        # 抓取代理
+        response = requests.get(
+            'http://www.xiladaili.com/gaoni/' + str(page + 1)+'/', timeout=10, proxies=proxies)
+        # 设置编码
+        response.encoding = 'utf-8'
+        # 解析
+        s = etree.HTML(response.text)
+        # 获取ip和请求类型
+        ips = s.xpath(
+            '/html/body/div[1]/div[3]/div[2]/table/tbody/tr/td[1]/text()')
+        types = s.xpath(
+            '/html/body/div[1]/div[3]/div[2]/table/tbody/tr/td[2]/text()')
+        if (len(ips) == 0):
+            print('抓取数据为空', row)
+            # 删除无效代理
+            ip_table.delete_one({"ip": row['ip'], "port": row['port']})
+        else:
+            print('抓取数据成功, 正在存入数据库...')
+            # 存储ip
+            for index, ip in enumerate(ips):
+                host = ip.split(':')[0]
+                port = ip.split(':')[1]
+                table.insert_one(
+                    {"ip": host, "port": port, "type": types[index]})
+    except:
+        print('超时', row)
+        # 删除无效代理
+        ip_table.delete_one({"ip": row['ip'], "port": row['port']})
+
+
+# 抓取网页的数量
+for page in range(0, 10):
     # 添加一个线程
     spider_poll.submit(getIp, (page))
--- ip/proving.py
+++ ip/proving.py
@@ -1,31 +1,36 @@
 import requests
 from pymongo import MongoClient
-from bson import ObjectId
 from concurrent.futures import ThreadPoolExecutor  # 线程池
 
-REQ_TIMEOUT = 5  # 超时时间
+REQ_TIMEOUT = 3  # 超时时间
 
 # 数据库连接
 client = MongoClient('127.0.0.1', 27017)
 db = client.ip  # 连接mydb数据库，没有则自动创建
-table = db.table  # 使用test_set集合，没有则自动创建
+table = db.table  # 所有的ip表
+ip_table = db.ip  # 有效ip表
 
+# 线程池
 spider_poll_max = 20  # 爬虫线程池最大数量
-proving_poll = ThreadPoolExecutor(
-    max_workers=spider_poll_max)  # 爬虫线程池 max_workers最大数量
+# 爬虫线程池 max_workers最大数量
+proving_poll = ThreadPoolExecutor(max_workers=spider_poll_max)
 
 
 def proving(ip):
-    global table, ObjectId
-    host = ip[0]
-    port = ip[1]
-    id = ip[2]
+    '''
+        @method 检测所有ip中有效的ip
+    '''
+    global table, ip_table
+    host = ip['ip']
+    port = ip['port']
+    _id = ip['_id']
+    types = ip['type']
 
     proxies = {
         'http': host+':'+port,
         'https': host+':'+port
     }
-    print(proxies)
+
     try:
         # 通过比较ip是否相同来判断代理是否有效
         OrigionalIP = requests.get(
@@ -34,20 +39,68 @@ def proving(ip):
                                 timeout=REQ_TIMEOUT, proxies=proxies).content
         # 删除代理
         if OrigionalIP == MaskedIP:
-            # table.delete_one({'_id': host, 'port': port})
-            table.delete_one({'_id': ObjectId(id)})
+            result = table.delete_one(
+                {"ip": host, "port": port, "type": types})
+            # print('删除成功', result.deleted_count)
         else:
-            print('通过')
+            print('新增有效代理', host+':'+port)
+            # 有效代理则存到ip表中
+            ip_table.insert_one({"ip": host, "port": port, "type": types})
     except:
         # 删除代理
-        # table.delete_one({'host': host, 'port': port})
-        table.delete_one({'_id': ObjectId(id)})
+        # print('删除成功')
+        result = table.delete_one({"ip": host, "port": port, "type": types})
+        # print(result.deleted_count)
 
 
-# 查询数据库的所有ip
-ips = table.find({}, {'ip': 1, 'port': 1})
+def proving_ip(ip):
+    '''
+        @method 检测有效ip中无效ip
+    '''
+    global ip_table
+    host = ip['ip']
+    port = ip['port']
+    _id = ip['_id']
+    types = ip['type']
 
+    proxies = {
+        'http': host+':'+port,
+        'https': host+':'+port
+    }
+
+    try:
+        # 通过比较ip是否相同来判断代理是否有效
+        OrigionalIP = requests.get(
+            "http://icanhazip.com", timeout=REQ_TIMEOUT).content
+        MaskedIP = requests.get("http://icanhazip.com",
+                                timeout=REQ_TIMEOUT, proxies=proxies).content
+        # 删除代理
+        if OrigionalIP == MaskedIP:
+            ip_table.delete_one(
+                {"ip": host, "port": port, "type": types})
+            # print('删除成功', result.deleted_count)
+        else:
+            print('有效代理', host+':'+port)
+
+    except:
+        # 删除代理
+        # print('删除成功')
+        ip_table.delete_one({"ip": host, "port": port, "type": types})
+        # print(result.deleted_count)
+
+
+# 先检测有效ip表中无效的ip
+proving_ips = ip_table.find({})
+print('开始清理无效ip...')
 # 有效性验证
-for index, data in enumerate(ips):
+for data in proving_ips:
     # 添加一个线程
-    proving_poll.submit(proving, ((data['ip'], data['port'], data['_id'])))
+    proving_poll.submit(proving_ip, (data))
+
+
+ips = table.find({})
+print('开始代理有效性验证...')
+# 有效性验证
+for data in ips:
+    # 添加一个线程
+    proving_poll.submit(proving, (data))
